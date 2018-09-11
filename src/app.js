1a1ca26d974bfff242a63d750d6e1f22fc3a11f2
yojinlll <yojin1995@qq.com>
2018-09-11 19:51:50 +0800

    6.添加 input 测试用例

--- src/app.js
+++ src/app.js
@@ -4,16 +4,21 @@ import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
-Vue.component('g-button-group',ButtonGroup)
-Vue.component('g-input',Input)
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 
 new Vue({
-    el:'#app',
-    data:{
+    el: '#app',
+    data: {
         loading1: false,
-        loading2: false,
+        loading2: true,
         loading3: false,
+    },
+    methods: {
+        inputChange (fuck) {
+            console.log(fuck.target.value)
+        }
     }
 })
---
+++ test/input.test.js
@@ -0,0 +1,109 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+
+    it('存在.', () => {
+        expect(Input).to.exist
+    })
+
+    describe('props', () => {
+        const Constructor = Vue.extend(Input)
+        let vm
+        afterEach(()=>{
+            vm.$destroy()
+        })
+
+        it('接收 value', () => {
+            vm = new Constructor({
+                propsData: {
+                    value: '1234'
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.value).to.equal('1234')
+        })
+
+        it('接收 disabled', () => {
+             vm = new Constructor({
+                propsData: {
+                    disabled: true
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.disabled).to.equal(true)
+        })
+
+        it('接收 readonly', () => {
+             vm = new Constructor({
+                propsData: {
+                    readonly: true
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.readOnly).to.equal(true)
+        })
+
+        it('接收 error', () => {
+             vm = new Constructor({
+                propsData: {
+                    icon: {},
+                    error: '你错了'
+                }
+            }).$mount()
+            const useElement = vm.$el.querySelector('use')
+            expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+            const errorMessage = vm.$el.querySelector('.errorMessage')
+            expect(errorMessage.innerText).to.equal('你错了')
+        })
+    })
+
+    describe('事件',()=>{
+        const Constructor = Vue.extend(Input)
+        let vm
+        afterEach(()=>{
+            vm.$destroy()
+        })
+
+        it('支持 change事件',()=>{
+            vm = new Constructor({}).$mount()
+            const callback = sinon.fake()
+            vm.$on('change',callback)
+            let event = new Event('change')
+            let inputElement = vm.$el.querySelector('input')
+            inputElement.dispatchEvent(event)
+            expect(callback).to.have.been.calledWith(event)
+        })
+        it('支持 input 事件',()=>{
+            vm = new Constructor({}).$mount()
+            const callback = sinon.fake()
+            vm.$on('input',callback)
+            let event = new Event('input')
+            let inputElement = vm.$el.querySelector('input')
+            inputElement.dispatchEvent(event)
+            expect(callback).to.have.been.calledWith(event)
+        })
+        it('支持 focus 事件',()=>{
+            vm = new Constructor({}).$mount()
+            const callback = sinon.fake()
+            vm.$on('focus',callback)
+            let event = new Event('focus')
+            let inputElement = vm.$el.querySelector('input')
+            inputElement.dispatchEvent(event)
+            expect(callback).to.have.been.calledWith(event)
+        })
+        it('支持 blur 事件',()=>{
+            vm = new Constructor({}).$mount()
+            const callback = sinon.fake()
+            vm.$on('blur',callback)
+            let event = new Event('blur')
+            let inputElement = vm.$el.querySelector('input')
+            inputElement.dispatchEvent(event)
+            expect(callback).to.have.been.calledWith(event)
+        })
+    })
+})
