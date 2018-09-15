dc7a7dfd34e81288c59f5658beadca151922332f
yojinlll <yojin1995@qq.com>
2018-09-15 23:27:30 +0800

    8.布局功能基本完成

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import ButtonGroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Sider from './sider'
+import Header from './header'
+import Content from './content'
+import Footer from './footer'
+
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -12,6 +18,11 @@ Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout',Layout)
+Vue.component('g-sider',Sider)
+Vue.component('g-header',Header)
+Vue.component('g-content',Content)
+Vue.component('g-footer',Footer)
 
 new Vue({
     el: '#app',
@@ -19,7 +30,7 @@ new Vue({
         loading1: false,
         loading2: true,
         loading3: false,
-        message: 'v-model'
+        message: 'v-model',
     },
     methods: {
         inputChange(e) {
---
+++ src/sider.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name:'BuluSider'
+
+    }
+</script>
+<style lang="scss" scoped>
+
+</style>
