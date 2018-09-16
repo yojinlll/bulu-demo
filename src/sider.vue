4d420d9b249457c338c3949abb656f1e73a7706b
yojinlll <yojin1995@qq.com>
2018-09-16 08:22:52 +0800

    8.添加简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,14 +1,36 @@
 <template>
-    <div class="sider">
-        <slot></slot>
-    </div>
+    <transition name="slide">
+        <div class="sider" v-if="visible">
+            <slot></slot>
+            <button @click="visible=false">close</button>
+        </div>
+    </transition>
 </template>
 <script>
     export default {
-        name:'BuluSider'
-
+        name:'BuluSider',
+        data(){
+            return {
+                visible:true
+            }
+        },
+        methods:{}
     }
 </script>
 <style lang="scss" scoped>
+    .sider{
+        position: relative;
+        > button {
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+    }
+    .slide-enter-active,.slide-leave-active {
+        transition: all .5s;
+    }
+    .slide-enter,.slide-leave-to {
+        margin-left: -200px;
+    }
 
 </style>
