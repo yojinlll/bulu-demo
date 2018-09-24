174eec672e882710e3fbd0120ed46b8a25a7242f
yojinlll <yojin1995@qq.com>
2018-09-24 15:52:44 +0800

    11.添加默认样式，且再完善click事件

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display: inline-block">
             <slot></slot>
         </span>
     </div>
@@ -28,6 +28,9 @@
                 if(this.$refs.popover &&
                     (this.$refs.popover === e.Target || this.$refs.popover.contains(e.Target))
                 ) { return }
+                if(this.$refs.contentWrapper &&
+                    (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))        // 该节点位置移动过
+                ){ return }
                 this.close()
             },
             open(){
@@ -58,6 +61,8 @@
 </script>
 
 <style lang="scss" scoped>
+    $border-color: #333;
+    $border-radius: 4px;
     .popover {
         display: inline-block;
         vertical-align: top;
@@ -66,8 +71,34 @@
 
     .content-wrapper {
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        filter: drop-shadow(0 1px 1px rgba(0,0,0,0.5));
+        background: white;
         transform: translateY(-100%);
+
+        margin-top: -10px;
+        padding: .5em 1em;
+        max-width: 20em;
+        word-break: break-all;
+
+        // 小三角样式
+        &::before, &::after {
+            content: '';
+            display: block;
+            border: 10px solid transparent;
+            width: 0;
+            height: 0;
+            position: absolute;
+            left: 10px;
+        }
+        &::before {
+            border-top-color: black;
+            top: 100%;
+        }
+        &::after {
+            border-top-color: white;
+            top: calc(100% - 1px);
+        }
     }
 </style>
