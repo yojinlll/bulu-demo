fbaaf439af29067ddb8cf0b03fc139ead147c13d
yojinlll <yojin1995@qq.com>
2018-09-26 22:39:00 +0800

    将 scss 变量提升到 var.scss

--- src/input.vue
+++ src/input.vue
@@ -41,17 +41,11 @@
 
 </script>
 <style lang="scss" scoped>
-    $height: 32px;
-    $border-color: #999;
-    $border-color-hover: #222;
-    $border-radius: 4px;
-    $font-size: 12px;
-    $box-shadow-color: rgba(0, 0, 0, 0.5);
-    $red: #f1453d;
+    @import "var";
     .wrapper { font-size: $font-size; display: inline-flex; align-items: center;
         > :not( last-child ){ margin-right: .5em; }
         > input {
-            height: $height;
+            height: 32px;
             border: 1px solid $border-color;
             border-radius: 4px;
             padding: 0 8px;
